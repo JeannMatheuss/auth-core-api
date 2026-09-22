3c163a4740219af078f53175dde64cd01efbd0bf
Jean Matheus <jeanmatheus0612@gmail.com>
2026-09-22 00:12:43 -0300

    AuthCore API

---
+++ src/main/java/br/com/auth_core_api/AuthCoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.auth_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/auth_core_api/AuthCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.auth_core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
