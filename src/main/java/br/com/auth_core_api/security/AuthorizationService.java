18d5c8602c803b7ffb60524a82eaf8524131ed1e
Jean Matheus <jeanmatheus0612@gmail.com>
2026-09-23 00:36:09 -0300

    arrumando o import de UserDetailsService

--- src/main/java/br/com/auth_core_api/security/AuthorizationService.java
+++ src/main/java/br/com/auth_core_api/security/AuthorizationService.java
@@ -3,17 +3,18 @@ package br.com.auth_core_api.security;
 import br.com.auth_core_api.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-
 @Service
-public class AuthorizationService implements UserDatailsService {
+public class AuthorizationService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
 
+
+    // Toda vez que alguém tentar logar, o Spring vai chamar esse método e nós dizemos a ele para procurar no nosso banco usando o repositório que criamos.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByLogin(username);
