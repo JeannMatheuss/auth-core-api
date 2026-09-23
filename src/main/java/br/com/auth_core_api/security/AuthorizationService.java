566a3de051518fa64dba54a5d5363f2b7d226531
Jean Matheus <jeanmatheus0612@gmail.com>
2026-09-23 00:33:12 -0300

    feat: criando o Service de autorização

---
+++ src/main/java/br/com/auth_core_api/security/AuthorizationService.java
@@ -0,0 +1,21 @@
+package br.com.auth_core_api.security;
+
+import br.com.auth_core_api.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Service
+public class AuthorizationService implements UserDatailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
